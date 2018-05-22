cc2742552079163bc4762aae4534223f170bc476
Jonathan Richman <cylindricalcow@gmail.com>
2018-05-22 03:11:33 -0500

    Zhat

--- spectral_clustering.py
+++ spectral_clustering.py
@@ -2,6 +2,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.datasets import make_blobs
+from sklearn.metrics.pairwise import pairwise_distances
+import scipy
+
 #Landmark based representation algorithm from https://int8.io/large-scale-spectral-clustering-with-landmark-based-representation/
 #Landmarks are vectors
 
@@ -44,4 +47,23 @@ if test1:
     find_p(X)
 
 #Compute Zhat
+def gaussian_kernel(dist_mat, bandwidth):
+    return np.exp(-dist_mat / (2*bandwidth**2))
+
+def compose_Sparse_ZHat_Matrix(X, landmarks, bandwidth, r):
+    dist_mat=pairwise_distances(X,landmarks)
+    sim_mat=gaussian_kernel(dist_mat, bandwidth)
     
+    Zhat = np.zeros(sim_mat.shape)
+
+    for i in range(Zhat.shape[0]):
+        #may need j.sort.selectperm
+        top_Landmarks_indices = np.argsort(-sim_mat[i,:])[:r]
+        top_Landmarks_coefs = sim_mat[i,top_Landmarks_indices]
+        top_Landmarks_coefs /= np.sum(top_Landmarks_coefs)
+        Zhat[i, top_Landmarks_indices] = top_Landmarks_coefs
+    #May be wrong    
+    diagm=np.sum(Zhat, axis=0)**(-1/2)
+    return diagm*Zhat
+if test1:
+    print(compose_Sparse_ZHat_Matrix(X, landmarks_kmeans, 1, 5))
